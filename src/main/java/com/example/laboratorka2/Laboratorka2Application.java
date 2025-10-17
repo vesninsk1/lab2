57cfc657183094a0c0eb38ee5366398d89161fda
vesninsk1 <svedmivik32@gmail.com>
2025-10-17 06:45:03 +0300

    Add files via upload

---
+++ src/main/java/com/example/laboratorka2/Laboratorka2Application.java
@@ -0,0 +1,13 @@
+package com.example.laboratorka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorka2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorka2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/laboratorka2/Laboratorka2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laboratorka2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorka2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
